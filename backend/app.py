ec381a02d9de3b0bae010da2c627705d0632c9c7
isasiti <isasiti06@gmail.com>
2026-06-08 23:07:04 -0500

    -

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,8 @@ from config import config
 import os
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-
+#Prueba
 # Si no encuentra la variable DB_HOST, por defecto usará 'localhost' (así te sigue funcionando a ti en tu PC)
 db_host = os.getenv('DB_HOST', 'localhost')
 def create_app(config_name='development'):
